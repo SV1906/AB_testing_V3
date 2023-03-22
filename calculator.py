3257fde8e870616625a2b4dcca1b5ee374e3f460
Sandhya Vinukonda <sandy.vinukonda@gmail.com>
2023-03-22 15:30:55 +0530

    Partially working UI

--- calculator.py
+++ calculator.py
@@ -37,7 +37,27 @@ def get_form_parameters():
     Form["Hypothesis"] = request.form['Hypothesis'] 
     Form["ConversionInterval"] = request.form['confidence_Interval_Input']
     Form["MarginError"] = request.form['margin_Error_Input']
+    Form['BaselineRate'] = request.form['baseline_Rate_Input']
+    Form['DetectableEffect'] = request.form['detectable_Effect_Input']
+    Form['SignificantPower'] = request.form['significance_Power_Input']
+    Form['SignificantLevel'] = request.form['significance_Level_Input']
+    Form['Operator'] = request.form['operator']
+    Form['CampaignName'] = request.form['campaign_name_input']
+    Form['CampaignStartdate'] = request.form['campaign_start_date']
+    Form['CampaignEnddate'] = request.form['campaign_end_date']
+    Form['CampaignType'] = request.form['Campaign_type_input']
+    Form['ConversionMetric'] = request.form['Conversion_metric_input']
+    Form['ConversionPeriod'] = request.form['Conversion_period']
+    # if request.form.get('campaign_name_input') :
+    #     Form['CampaignName'] = request.form['campaign_name_input']
+        
+        
+    # Form['CampaignStartdate'] = request.form['campaign_start_date']
+
     return Form
+
+    #Form['SamplingTechnique']= request.form['operator']  
+    
 
 
 def basic_Sample_Size(x,y):
@@ -47,35 +67,161 @@ def basic_Sample_Size(x,y):
     z_score_CI = round(stats.norm.ppf(1-(1-CI)/2),2) 
 
     result = round(0.25/math.pow((MOE/z_score_CI),2))
-
-    result_array = [result]
     return result
 
+def evan_Millers(input1, input2, input3, input4):
+    input1 = float(input1)
+    input2 = float(input2)
+    input3 = float(input3)
+    input4 = float(input4)
+    if (input1 > 49):
+        input1 = 100-input1
+    p1 = input1/100
+    p2 = (input2+input1)/100
+    alpha = (input4/100)/2 
+    beta = 1- (input3/100)
+    z_score_alpha = stats.norm.ppf(1-alpha)
+    z_score_beta = stats.norm.ppf(1-beta)
+    part_1 = z_score_alpha*math.sqrt((2*p1*(1-p1)))
+    part_2 = z_score_beta*math.sqrt(p1*(1-p1) + p2*(1-p2))
+    deno = math.pow(p2 - p1, 2) 
+    n = (math.pow((part_1+part_2),2))/(deno)
+    result = round(n)
+    return result 
 
-# def basic_Sample_Size(): 
-#     error = None
-#     result = None
+#First input is the type and the second input is the sample size 
+def sampling_technique(first_input):
 
-#     input1 = float(request.form['Input1'])
-#     input2 = float(request.form['Input1'])
-#     input3 = float(request.form['Input1'])
-#     input4 = float(request.form['Input1']) 
+    input1 = first_input
+    sample_size = int(100)
+    THIS_FOLDER = Path(__file__).parent.resolve()
+    final_data = str(THIS_FOLDER)+"\\Blood Transfusion Service Centre Dataset.csv"
+    data = pd.read_csv(final_data)
 
-#     if (input1 > 49):
-#         input1 = 100-input1
-#     p1 = input1/100
-#     p2 = (input2+input1)/100
-#     alpha = (input4/100)/2 
-#     beta = 1- (input3/100)
-#     z_score_alpha = stats.norm.ppf(1-alpha)
-#     z_score_beta = stats.norm.ppf(1-beta)
-#     part_1 = z_score_alpha*math.sqrt((2*p1*(1-p1)))
-#     part_2 = z_score_beta*math.sqrt(p1*(1-p1) + p2*(1-p2))
-#     deno = math.pow(p2 - p1, 2) 
-#     n = (math.pow((part_1+part_2),2))/(deno)
-#     result = round(n)
-#     result_array = [result]
-#     return result_array 
+    if (input1 == "SR"):
+        #data = pd.read_csv(r"C:\Users\Sandhya\OneDrive\Desktop\AB Testing_Folder\ABTesting_v2\ABTesting_V2.0\AB_testing_V3\Blood Transfusion Service Centre Dataset.csv")
+        #sample_size = 100
+        def Random_sample(data,sample_size): 
+           new_data_random = pd.DataFrame()
+           #sample_size = sample_size 
+           file = ""
+           for i in range(sample_size): 
+                 number = random.randint(1,data.shape[0])
+                 new_data_random = pd.concat([new_data_random,data.iloc[number:number+1]])
+           #path = "C:\\Users\\Sandhya\\OneDrive\\Desktop\\AB Testing_Folder\\ABTesting_v2\\ABTesting_V2.0\\AB_testing_V3"
+           file = new_data_random.to_csv(str(THIS_FOLDER)+"\\result_RandomSample.csv") 
+           if (os.path.exists(str(THIS_FOLDER)+"\\result_SystematicSample.csv")): 
+                os.remove(str(THIS_FOLDER)+"\\result_SystematicSample.csv")
+           if (os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")): 
+               os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")      
+           if (os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")): 
+               os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")      
+           return file 
+        Random_sample(data,sample_size)
+        result = sample_size
+
+    elif (input1 == "SyC"):
+        #data = pd.read_csv(r"C:\Users\Sandhya\OneDrive\Desktop\AB Testing_Folder\ABTesting_v2\ABTesting_V2.0\AB_testing_V3\Blood Transfusion Service Centre Dataset.csv")
+        #sample_size = 100 
+        def Systematic_sample(data,sample_size): 
+             #sample_size = sample_size
+             k = int (data.shape[0]/sample_size)
+             new_data_systematic = pd.DataFrame()
+             for i in range(1, data.shape[0],k):
+                new_data_systematic = pd.concat([new_data_systematic,data.iloc[i:i+1]])
+                if(new_data_systematic.shape[0]==sample_size): 
+                 break 
+             #path = "C:\\Users\\Sandhya\\OneDrive\\Desktop\\AB Testing_Folder\\ABTesting_v2\\ABTesting_V2.0\\AB_testing_V3"
+             file = new_data_systematic.to_csv(str(THIS_FOLDER)+"\\result_SystematicSample.csv") 
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")                 
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_RandomSample.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_RandomSample.csv")                
+             return file 
+             #return new_data_systematic
+        Systematic_sample(data,sample_size)
+
+    elif (input1 == "StCK") :
+        #data = pd.read_csv(r"C:\Users\Sandhya\OneDrive\Desktop\AB Testing_Folder\ABTesting_v2\ABTesting_V2.0\AB_testing_V3\Blood Transfusion Service Centre Dataset.csv")
+        #sample_size = 100 
+        def Stratified_sample_KMeans(data,sample_size):
+             new_data_stratified = data
+             sse = []
+             #To check the elbow of Kmeans 
+             for i in range(2,11):
+                  kmeans = KMeans(n_clusters=i)
+                  kmeans.fit(new_data_stratified)
+                  sse.append(kmeans.inertia_)
+             kl = KneeLocator(range(2, 11), sse, curve="convex", direction="decreasing")
+             #To create the final clusters 
+             kmeans = KMeans(n_clusters=kl.elbow)
+             kmeans.fit(new_data_stratified)
+             new_data_stratified["label"] = list(kmeans.labels_)
+             l_num = {}
+             for i in range(0,kl.elbow): 
+               l_num[i] = (round(((len(new_data_stratified[new_data_stratified.label==i])/new_data_stratified.shape[0])*sample_size)))
+              # print(kl.elbow)
+             l_data = {}
+             for i in  range(0,kl.elbow): 
+               l_data[i] = new_data_stratified[new_data_stratified.label==i].groupby('label', group_keys=False).apply(lambda x: x.sample(l_num[i]))
+             final_data = []
+             final_data = pd.concat([l_data[0],l_data[1],l_data[2],l_data[3]])
+             #Random_sample(data,sample_size).to_csv('C:/Users/Sandhya/Downloads/result.csv')
+             #path = "C:\\Users\\Sandhya\\OneDrive\\Desktop\\AB Testing_Folder\\ABTesting_v2\\ABTesting_V2.0\\AB_testing_V3"
+             file = final_data.to_csv(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_SystematicSample.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_SystematicSample.csv")
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_RandomSample.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_RandomSample.csv")       
+             if (os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")): 
+                 os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")                             
+             return file 
+             #return final_data
+        Stratified_sample_KMeans(data,sample_size)
+
+    else : 
+        #data = pd.read_csv(r"C:\Users\Sandhya\OneDrive\Desktop\AB Testing_Folder\ABTesting_v2\ABTesting_V2.0\AB_testing_V3\Blood Transfusion Service Centre Dataset.csv")
+        #sample_size = 100 
+        def Stratified_sample_Birch(data, sample_size):
+            model = Birch()
+            model.fit(data)
+            labels = model.predict(data)
+            #To add the column labels to the dataframe available 
+            data['labels']= labels 
+            #To find the total number of clusters in the dataset from the column - labels 
+            total = len(data.labels.value_counts())
+            #To find how many number of clusters are required in the sample size based on the percentage of the actual dataset in the vairble l_num
+            l_num = {}
+            for i in range(0,total-1): 
+                l_num[i] = (round(((len(data[data.labels==i])/data.shape[0])*sample_size)))
+            #To find the 
+            l_data = {}
+            #To declare the variable new_array for the record of the clusters who's value in the sample data is not zero
+            new_array = []
+            for i in  range(0,total-1): 
+                if(l_num[i]!=0):
+                 #To sample data which is not zero using the if function 
+                    l_data[i] = data[data.labels==i].groupby('labels').apply(lambda x: x.sample(l_num[i]))
+                    new_array.append(i)
+                 #To concat all the variable into the final data for the final sample dataset 
+            final_data = []
+            final_data = pd.concat([l_data[i] for i in new_array], ignore_index = True)
+            #return final_data
+            #path = "C:\\Users\\Sandhya\\OneDrive\\Desktop\\AB Testing_Folder\\ABTesting_v2\\ABTesting_V2.0\\AB_testing_V3"
+            file = final_data.to_csv(str(THIS_FOLDER)+"\\result_StratifiedSample_BIRCH.csv")
+            if (os.path.exists(str(THIS_FOLDER)+"\\result_SystematicSample.csv")): 
+               os.remove(str(THIS_FOLDER)+"\\result_SystematicSample.csv")
+            if (os.path.exists(str(THIS_FOLDER)+"\\result_RandomSample.csv")): 
+               os.remove(str(THIS_FOLDER)+"\\result_RandomSample.csv")   
+            if(os.path.exists(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")):   
+               os.remove(str(THIS_FOLDER)+"\\result_StratifiedSample_KMeans.csv")                    
+            return file 
+        Stratified_sample_Birch(data,sample_size)
+        
+
+
 
 #To display the intial page 
 @Flask_App.route('/', methods=['GET'])
@@ -101,21 +247,25 @@ def New_Testing():
         
        if (forms["ConversionInterval"] != '' and forms["MarginError"] != ''):
             forms ["basic_result"] = basic_Sample_Size(forms["ConversionInterval"], forms["MarginError"])
+
+       if (forms["BaselineRate"] != '' and forms["DetectableEffect"] != '' and forms["SignificantPower"] != '' and forms["SignificantLevel"] != ''): 
+           forms ["evan_millers"] =  evan_Millers(forms["BaselineRate"], forms["DetectableEffect"], forms["SignificantPower"], forms["SignificantLevel"])
+        
+    #    if(forms['operator'] != ''):
+    #        sampling_technique(forms['operator'])
+  
+
     #    main_form["Hypothesis"] =  get_form_parameters("Hypothesis")
-
-
     #    for i in Features:
     #             if  'value' in i:  
     #                 i['value'] = 'True' 
-
-       
-
     #        else : 
     #            Features_input[i] = False
     #    main_form["Features"] = Features_input
-       return render_template('New_Testing.html',    Success = True, form = forms, features = Features, stratification_columns=Stratification_columns, date = today) 
 
-    return render_template('New_Testing.html' ,    features = Features, stratification_columns=Stratification_columns, date = today)
+       return render_template('New_Testing.html', Success = True, form = forms, features = Features, stratification_columns=Stratification_columns, date = today) 
+
+    return render_template('New_Testing.html' , features = Features, stratification_columns=Stratification_columns, date = today)
 
        
     #    if (request.form.get("baseline_Rate_Input")) and (request.form.get("detectable_Effect_Input")) and (request.form.get("significance_Power_Input")) and (request.form.get("significance_Level_Input")) : 
@@ -256,7 +406,7 @@ def suggesting_method():
         return render_template(
            'New_Testing.html',
             section = 'section_sampling',
-            Sample_Suggested = "Sampling technique : " + Sample,
+            Sample_Suggested = "Sampling technique : Systematic ",
             features = Features, stratification_columns=Stratification_columns, 
             date = today
         )
@@ -356,7 +506,7 @@ def User_Input():
     Campaign_end = request.form['campaign_end_date']
     Conversion_metric = request.form['Conversion_metric_input']
     Campaign_type = request.form['Campaign_type_input']  
-    Date_of_requirement = request.form['Date_of_requirement']
+    #Date_of_requirement = request.form['Date_of_requirement']
 
 
     THIS_FOLDER = Path(__file__).parent.resolve()
@@ -368,7 +518,7 @@ def User_Input():
     array_output = []
 
     array_output.append(index_len)
-    array_output.append(Date_of_requirement)
+    #array_output.append(Date_of_requirement)
     array_output.append(Campaign_name)
     array_output.append(Campaign_type)
     array_output.append(Campaign_start)
@@ -744,9 +894,7 @@ def sampling_select():
             features = Features,
             stratification_columns=Stratification_columns, 
             date = today
-        )
-        
-
+        )      
     else:
         def Systematic_sample(data,sample_size): 
              #sample_size = sample_size
@@ -779,5 +927,5 @@ def sampling_select():
 
 if __name__ == '__main__':
     Flask_App.debug = True
-    Flask_App.run(port= 5087)   
+    Flask_App.run(port= 5090)   
     
