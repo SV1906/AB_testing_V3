d30fa5b4ad998d5a55d560ec58f82eb44f7c5f22
Sandhya Vinukonda <sandy.vinukonda@gmail.com>
2023-04-13 11:03:58 +0530

    Lastest Stable Version

--- calculator.py
+++ calculator.py
@@ -57,7 +57,7 @@ def excel_update(forms, selected_features,test_size, selected_columns, today, Su
     file = (str(THIS_FOLDER)+"\\Master_Sheet.csv")
     df = pd.read_csv(file, on_bad_lines='skip')
     index_len = len(df)+1
-    array_output = [index_len,forms["Hypothesis"],forms['DOE'],selected_features,forms["final_result"],test_size,forms["Operator"],selected_columns,forms["CampaignName"], Sub_Campaign_Names, today, forms['CampaignStartdate'], forms['CampaignEnddate'], forms['CampaignType'], forms['ConversionMetric'], forms['ConversionPeriod']]
+    array_output = [index_len,forms["Hypothesis"],forms['Experiment'],selected_features,forms["final_result"],test_size,forms["Operator"],selected_columns,forms["CampaignName"], Sub_Campaign_Names, today, forms['CampaignStartdate'], forms['CampaignEnddate'], forms['CampaignType'], forms['ConversionMetric'], forms['ConversionPeriod']]
     with open(file, 'a' , newline ='') as f_object:
           writer_object = writer(f_object)
           writer_object.writerow(array_output)
@@ -70,15 +70,15 @@ def get_form_parameters():
 
          
     Form['Hypothesis'] = request.form['Hypothesis'] if (request.form.get('Hypothesis') != None) else ''
-    Form['DOE'] = request.form['Doe_input'] if (request.form.get('Doe_input') != None) else 0
+    Form['Experiment'] = request.form['Experiment_input'] if (request.form.get('Experiment_input') != None) else 0
     Form['ConversionInterval'] = request.form['confidence_Interval_Input'] if (request.form.get('confidence_Interval_Input') != None) else 0
     Form["MarginError"] = request.form['margin_Error_Input'] if (request.form.get('margin_Error_Input') != None) else 0
-    Form['Button'] = request.form['Button_id'] if (request.form.get('Button_id') != None) else ''
+    # Form['Button'] =  if (request.form.get('Button_id') != None) else ''
     Form['BaselineRate'] = request.form['baseline_Rate_Input'] if (request.form.get('baseline_Rate_Input') != None) else 0
     Form['DetectableEffect'] = request.form['detectable_Effect_Input'] if (request.form.get('detectable_Effect_Input') != None) else 0
     Form['SignificantPower'] = request.form['significance_Power_Input'] if (request.form.get('significance_Power_Input') != None) else 0
     Form['SignificantLevel'] = request.form['significance_Level_Input'] if (request.form.get('significance_Level_Input') != None) else 0
-    Form["Total_cases"] = int(Form['DOE'])+1
+    Form["Total_cases"] = int(Form['Experiment'])+1
     Form['Operator'] = 'Default'
     if (request.form.get('operator') != None) : 
         Form['Operator'] = request.form['operator']
@@ -89,7 +89,7 @@ def get_form_parameters():
     Form['ConversionMetric'] = request.form['Conversion_metric_input'] if (request.form.get('Conversion_metric_input') != None) else 'Retention rate'
     Form['ConversionPeriod'] = request.form['Conversion_period_input'] if (request.form.get('Conversion_period_input') != None) else ''
     Form['Sum'] = 0
-    # for i in range(1,int(Form["DOE"])+1):
+    # for i in range(1,int(Form["Experiment"])+1):
     #     variable = "Test_case_" + str(i)
     #     if (request.form.get(variable) != None and request.form.get(variable) != '') : 
     #        Form[variable] = request.form[variable] 
@@ -98,7 +98,7 @@ def get_form_parameters():
     #         Form[variable] = Form["final_result"]
       
 
-    # for i in range(1,int(Form["DOE"])+1):
+    # for i in range(1,int(Form["Experiment"])+1):
     #     variable = "Campaign_Name_" + str(i)
     #     if (request.form.get(variable) != None and request.form.get(variable) != '') : 
     #        Form[variable] = request.form[variable] 
@@ -138,7 +138,9 @@ def db_count(data,selected):
     
 def Output_file(data_list):
     try : 
-        with pd.ExcelWriter("C:\\Users\\Sandhya\\Downloads\\output.xlsx") as writer:
+       
+        # with pd.ExcelWriter("C:\\Users\\Sandhya\\Downloads\\output.xlsx") as writer:
+        with pd.ExcelWriter(str(THIS_FOLDER)+"\\output.xlsx") as writer:
             for i in range(0,len(data_list)):
                 data_list[i].to_excel(writer, sheet_name= "Sheet " + str(i+1))
         return True 
@@ -221,8 +223,8 @@ def Stratification_result(data, test_size,Selected, minimum_size):
             return "Sampling Successful but problem with saving in file."
     except : 
         if (loop_flag > 0): 
-            # return "Reduce the number of DOEs to atmost " + str(loop_flag) +" or change the columns to be stratified on "
-            return "Reduce the number of DOEs or change the columns to be stratified on "
+            # return "Reduce the number of Experiments to atmost " + str(loop_flag) +" or change the columns to be stratified on "
+            return "Reduce the number of Experiments or change the columns to be stratified on "
         else : 
             return "The least populated class in y has only 1 member. Please change the columns to be stratified on"
      
@@ -315,7 +317,7 @@ def New_Testing():
            forms ["evan_millers"] =  evan_Millers(forms["BaselineRate"], forms["DetectableEffect"], forms["SignificantPower"], forms["SignificantLevel"])
            forms["final_result"] = compare_size(forms["evan_millers"],forms["basic_result"])
 
-       for i in range(1,int(forms["DOE"])+1):
+       for i in range(1,int(forms["Experiment"])+1):
              variable = "Test_case_" + str(i)
              if (request.form.get(variable) != None and request.form.get(variable) != '') : 
                  forms[variable] = request.form[variable] 
@@ -323,10 +325,10 @@ def New_Testing():
              else : 
                 forms[variable] = forms["final_result"]
 
-       if (forms['DOE'] == 1): 
+       if (int(forms['Experiment']) == 1): 
             Sub_Campaign_Names.append(forms['CampaignName'])
        
-       for i in range(1,int(forms["DOE"])+1):
+       for i in range(1,int(forms["Experiment"])+1):
              variable = "Campaign_Name_" + str(i)
              if (request.form.get(variable) != None and request.form.get(variable) != '') : 
                     forms[variable] = request.form[variable] 
@@ -341,10 +343,10 @@ def New_Testing():
                  selected_features.append(i['key'])
 
 
-    #   if (forms['DOE'] == 1): 
+    #   if (forms['Experiment'] == 1): 
     #         Sub_Campaign_Names.append(forms['CampaignName'])
     #    else : 
-    #         for i in range(1,int(forms["DOE"])+1):
+    #         for i in range(1,int(forms["Experiment"])+1):
     #             variable = "Campaign_Name_" + str(i)
     #             if (request.form.get(variable) != None and request.form.get(variable) != '') : 
     #                     Sub_Campaign_Names.append(request.form[variable])
@@ -375,11 +377,11 @@ def New_Testing():
        forms["Current_Count"] = db_count(data_Original, selected_features)
 
        global button_variable 
-       if (forms["Button"] != None or button_variable == "True"):
+       if (request.form.get('Button_id') != None or button_variable == "True"):
            button_variable = "True"
            forms["Sample_suggestion"] = sample_suggest()
         
-       for i in range(1,int(forms["DOE"])+1):
+       for i in range(1,int(forms["Experiment"])+1):
             variable = "Test_case_" + str(i)
             if (request.form.get(variable) == None or request.form.get(variable) == '' or int(request.form.get(variable))< int(forms["final_result"])):
                 forms[variable] = forms["final_result"]
@@ -389,8 +391,8 @@ def New_Testing():
                         
        if (forms['final_result'] != 0 and forms['Current_Count'] != ''):
             Section_open = 3
-            verification = verification_func(forms["DOE"], forms["final_result"], forms["Current_Count"])
-            forms["required_db"] = requiredDB(forms["DOE"],forms["final_result"])
+            verification = verification_func(forms["Experiment"], forms["final_result"], forms["Current_Count"])
+            forms["required_db"] = requiredDB(forms["Experiment"],forms["final_result"])
             if (verification == "False"):
                     forms["Records_Available"] = forms['Current_Count'] - forms['final_result'] - forms['Sum']
                     if (forms["Records_Available"] < 0): 
@@ -405,7 +407,7 @@ def New_Testing():
 
        if (forms["CampaignName"] != '' and forms["CampaignStartdate"] != '' and forms["CampaignEnddate"] != '' and forms["CampaignType"] != '' and forms["ConversionMetric"] != '' and forms["ConversionPeriod"] != '' and request.form.get("Final_submit") != None):           
             forms["Campaign_Details_Error"] = ""
-            if (len(Sub_Campaign_Names) != int(forms["DOE"]) and int(forms["DOE"]) != 1): 
+            if (len(Sub_Campaign_Names) != int(forms["Experiment"]) and int(forms["Experiment"]) != 1): 
                 forms["Campaign_Details_Error"] = "Please fill in all the Sub Campaign Names"
             if (datetime.strptime(forms["CampaignStartdate"], '%Y-%m-%d') > datetime.strptime(forms["CampaignEnddate"], '%Y-%m-%d')):
                forms["Campaign_Details_Error"] = "The Campaign End Date should be after the Campaign Start DateStart date"
@@ -419,7 +421,7 @@ def New_Testing():
                 Section_open = 5
        
        if (request.form.get("Download_button") != None): 
-            path = "C:\\Users\\Sandhya\\Downloads\\output.xlsx"
+            path = (str(THIS_FOLDER)+"\\output.xlsx")
             return send_file(path, as_attachment=True)
 
        return render_template('New_Testing.html', Sub_Campaign_Names = Sub_Campaign_Names, Section_open = Section_open, stratification_columns = Stratification_columns, selected_columns =selected_columns, sum = sum, Success = True, form = forms, features = Features, date = today, Verification =  verification) 
@@ -434,5 +436,5 @@ def New_Testing():
   
 if __name__ == '__main__':
     Flask_App.debug = True
-    Flask_App.run(port= 5127)   
+    Flask_App.run(port= 5128)   
     
